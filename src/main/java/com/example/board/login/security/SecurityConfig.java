0bf0d91ed1d9692225322e3a84461bc6a6e17378
unknown <dabinh@eluon.com>
2022-09-21 17:47:48 +0900

    view 추가

--- src/main/java/com/example/board/login/security/SecurityConfig.java
+++ src/main/java/com/example/board/login/security/SecurityConfig.java
@@ -35,8 +35,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .addFilterBefore()
         http
                 .cors()
                 .and()
